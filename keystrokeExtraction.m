6a6f4aa057056839f121cfaa288296865ea67b55
Jesse Hogan Hoskins <hoskin24@egr.msu.edu>
2018-11-26 17:22:00 -0500

    Ready to rock and roll

--- keystrokeExtraction.m
+++ keystrokeExtraction.m
@@ -3,6 +3,7 @@ function [dummy_phase] = keystrokeExtraction(data, nummax, nummin, col)
 
 %find maxes
 selector = data(:,col);
+length(selector)
 %find nummax+nummin abs(biggest) components
 [maxval, idx_max]=maxk(selector,nummax);
 max_mat = [idx_max maxval];
@@ -10,12 +11,12 @@ max_mat = [idx_max maxval];
 min_mat = [idx_min minval];
 tokillidx=find(maxval<(mean(maxval)+std(maxval)));
 max_mat(tokillidx,:)=[];
-dummy_phase=zeros(length(selector));
+dummy_phase=zeros(length(selector),1);
 dummy_phase(max_mat(:,1))=max_mat(:,2);
 
 tokillidx_min=find(minval>(mean(minval)-std(minval)));
 min_mat(tokillidx_min,:)=[];
 dummy_phase(min_mat(:,1))=min_mat(:,2);
-
+        
 end
 
--- newMain.m
+++ newMain.m
@@ -13,128 +13,62 @@
 % 6. Write paper
 
 
-addpath('..\KamRFID\rfidtouch\Matlab_Processing')
+
 %% Playing with Kam's code
 clear all
 %close all
 clc
 
-file = '123Pin_1.csv';
-%file = '../tagTap/test/123Pin_0.csv';
+file = '123Pin_2.csv';
+%call the function to calc radial dist
+[phase_out, time_out] = DanFunc(file, 1);
+%delete last timestamp due to the diff on dist
+time_out(end,:)=[];
 
-% [OutDist_Phase_mat, OutDist_Dop_mat, MinPhaseOutLength, MinDopOutLength] = KamFunc(filename, ant_num)
-%[phase_out, dop_out, phaselength, doplength] = KamFunc('../tagTap/test/123Pin_control.csv', 2);
-%[phase_out, dop_out, phaselength, doplength] = KamFunc('../tagTap/test/123Pin_1.csv', 1);
-%[phase_out1, dop_out, phaselength1, doplength] = KamFunc(file, 1);
-%[phase_out2, phaselength2] = KamFuncTrim(file, 1);
-[phase_out] = DanFunc(file, 1);
+%% Plot the Radial dist values
+%make a copy to avoid messing with output
 mat1=phase_out;
 figure;
 hold on
-plot(mat1(:,1), 'r')
-title('Pre Filtered Phase - Tag 1 Antenna 1')
+plot(time_out(:,1), mat1(:,1), 'r')
+title('Radial Dist - Tag 1')
 hold off
 
 figure;
 hold on
-plot(mat1(:,2), 'b')
-title('Pre Filtered Phase - Tag 2 Antenna 1')
+plot(time_out(:,2), mat1(:,2), 'b')
+title('Radial Dist - Tag 2')
 hold off
 
 figure;
 hold on
-plot(mat1(:,3), 'g')
-title('Pre Filtered Phase - Tag 3 Antenna 1')
+plot(time_out(:,3), mat1(:,3), 'g')
+title('Radial Dist - Tag 3')
 hold off
-
-% error check, lots of values are about x*pi away from line. Fix them
-%subtract pi from values in array
-% piVal = 2.5;
-% for i = 1:length(phase_out)
-%     phaseDiff = phase_out(i,3);
-%     %above
-%     if (phase_out(i,3) > 2*piVal)
-%         phase_out(i,3) = phase_out(i,3) - 2*pi;
-%     elseif (phase_out(i,3) > piVal)
-%         phase_out(i,3) = phase_out(i,3) - pi;
-%     %below
-%     elseif (phase_out(i,3) < -2*piVal)
-%         phase_out(i,3) = phase_out(i,3) + 2*pi;
-%     elseif (phase_out(i,3) < -piVal)
-%         phase_out(i,3) = phase_out(i,3) + pi;
-%     end
-% end
-
-
-
-
-mat1 = phase_out(phase_out(:,1)==1,:);
-mat2 = phase_out(phase_out(:,1)==2,:);
-mat3 = phase_out(phase_out(:,1)==3,:);
-
-
-
-% windowSize = 5;
-% b = (1/windowSize)*(ones(1,windowSize));
-% a = 5;
-% 
-% mat1 = filter(b,a,mat1);
-% mat2 = filter(b,a,mat2);
-% mat3 = filter(b,a,mat3);
 
 
 figure;
 hold on
-plot(mat1(:,4), mat1(:,3), 'r')
-plot(mat2(:,4), mat2(:,3), 'g')
-plot(mat3(:,4), mat3(:,3), 'b')
-%plot(mat1(:,4), 'r')
-%plot(mat1(:,5), 'b')
-%plot(mat1(:,6), 'b')
-title('Plot by Timestamp')
+plot(time_out(:,1), mat1(:,1), 'r')
+plot(time_out(:,2), mat1(:,2), 'b')
+plot(time_out(:,3), mat1(:,3), 'g')
+title('Radial Dist - All Tags')
 legend('Tag 1', 'Tag 2', 'Tag 3');
-%legend('Tag 2 Ant 1', 'Tag 2 Ant 2', 'Tag 5 Ant 1', 'Tag 5 Ant 2', 'Tag 8 Ant 1', 'Tag 8 Ant 2');
 hold off
 
-mat11 = diff(mat1);
-mat22 = diff(mat2);
-mat33 = diff(mat3);
 
+%% Perform keystroke extraction
+out1 = keystrokeExtraction(phase_out, 70, 70, 1);
+out2 = keystrokeExtraction(phase_out, 70, 70, 2);
+out3 = keystrokeExtraction(phase_out, 70, 70, 3);
 figure;
 hold on
-plot(mat1(2:end,4), mat11(:,3), 'r')
-plot(mat2(2:end,4), mat22(:,3), 'g')
-plot(mat3(2:end,4), mat33(:,3), 'b')
-title('Plot by Timestamp - Diff')
+plot(time_out(:,1), out1, 'r')
+plot(time_out(:,2), out2, 'b')
+plot(time_out(:,3), out3, 'g')
 legend('Tag 1', 'Tag 2', 'Tag 3');
-%legend('Tag 2 Ant 1', 'Tag 2 Ant 2', 'Tag 5 Ant 1', 'Tag 5 Ant 2', 'Tag 8 Ant 1', 'Tag 8 Ant 2');
+title('Antenna 1');
 hold off
-
-
-
-%every time the wavelength shifts,
-%subtract new baseline from following
-%values
- 
-figure;
-hold on
-plot(mat1(:,4), mat1(:,3), 'r')
-title('Tag 1')
-hold off
-
-figure;
-hold on
-plot(mat2(:,4), mat2(:,3), 'g')
-title('Tag 2')
-hold off
-
-figure;
-hold on
-plot(mat3(:,4), mat3(:,3), 'b')
-title('Tag 3')
-hold off
-
-
 
 
 %% Sidenotes
@@ -172,3 +106,22 @@ hold off
 % plot(c(:,3),c(:,2))
 % xlim([0 30])
 % ylim([0 4])
+
+
+% error check, lots of values are about x*pi away from line. Fix them
+%subtract pi from values in array
+% piVal = 2.5;
+% for i = 1:length(phase_out)
+%     phaseDiff = phase_out(i,3);
+%     %above
+%     if (phase_out(i,3) > 2*piVal)
+%         phase_out(i,3) = phase_out(i,3) - 2*pi;
+%     elseif (phase_out(i,3) > piVal)
+%         phase_out(i,3) = phase_out(i,3) - pi;
+%     %below
+%     elseif (phase_out(i,3) < -2*piVal)
+%         phase_out(i,3) = phase_out(i,3) + 2*pi;
+%     elseif (phase_out(i,3) < -piVal)
+%         phase_out(i,3) = phase_out(i,3) + pi;
+%     end
+% end
